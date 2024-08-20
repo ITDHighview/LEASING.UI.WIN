97a41083ec86e348c7c5363211308e112ebe361e
xgenboy27 <80162474+xgenboy27@users.noreply.github.com>
2024-08-19 17:37:56 -0700

    update08192024
    Total Amount

--- SCRIPTS/NEW UPDATES/07282024/[sp_GetNotificationList].sql
+++ SCRIPTS/NEW UPDATES/07282024/[sp_GetNotificationList].sql
@@ -12,12 +12,12 @@ AS
     BEGIN
 
         SELECT
-                [tblClientMstr].[ClientName]                                        AS [Client],
-                [tblUnitReference].[ClientID]                                       AS [ClientID],
-                [tblUnitReference].[RefId]                                          AS [ContractID],
-                CONVERT(VARCHAR(15), [tblMonthLedger].[LedgMonth], 107)             AS [ForMonth],
-                FORMAT(CAST([tblMonthLedger].[LedgAmount] AS DECIMAL(18, 2)), 'N2') AS [Amount],
-                'HOLD'                                                              AS [Status]
+                [tblClientMstr].[ClientName]                                                                                 AS [Client],
+                [tblUnitReference].[ClientID]                                                                                AS [ClientID],
+                [tblUnitReference].[RefId]                                                                                   AS [ContractID],
+                CONVERT(VARCHAR(15), [tblMonthLedger].[LedgMonth], 107)                                                      AS [ForMonth],
+                FORMAT((ISNULL([tblMonthLedger].[LedgRentalAmount], 0) + ISNULL([tblMonthLedger].[PenaltyAmount], 0)), 'N2') AS [Amount],
+                'HOLD'                                                                                                       AS [Status]
         FROM
                 [dbo].[tblUnitReference] WITH (NOLOCK)
             LEFT JOIN
@@ -31,12 +31,12 @@ AS
                 AND CONVERT(VARCHAR(10), [tblMonthLedger].[LedgMonth], 103) > CONVERT(VARCHAR(10), GETDATE(), 103)
         UNION
         SELECT
-                [tblClientMstr].[ClientName]                                        AS [Client],
-                [tblUnitReference].[ClientID]                                       AS [ClientID],
-                [tblUnitReference].[RefId]                                          AS [ContractID],
-                CONVERT(VARCHAR(15), [tblMonthLedger].[LedgMonth], 107)             AS [ForMonth],
-                FORMAT(CAST([tblMonthLedger].[LedgAmount] AS DECIMAL(18, 2)), 'N2') AS [Amount],
-                'DUE'                                                               AS [Status]
+                [tblClientMstr].[ClientName]                                                                                 AS [Client],
+                [tblUnitReference].[ClientID]                                                                                AS [ClientID],
+                [tblUnitReference].[RefId]                                                                                   AS [ContractID],
+                CONVERT(VARCHAR(15), [tblMonthLedger].[LedgMonth], 107)                                                      AS [ForMonth],
+                FORMAT((ISNULL([tblMonthLedger].[LedgRentalAmount], 0) + ISNULL([tblMonthLedger].[PenaltyAmount], 0)), 'N2') AS [Amount],
+                'DUE'                                                                                                        AS [Status]
         FROM
                 [dbo].[tblUnitReference] WITH (NOLOCK)
             LEFT JOIN
@@ -49,12 +49,20 @@ AS
                 (
                     ISNULL([tblMonthLedger].[IsHold], 0) = 1
                     AND ISNULL([tblMonthLedger].[IsPaid], 0) = 0
+                    AND ISNULL([tblUnitReference].[IsSignedContract], 0) = 1
+                    AND ISNULL([tblUnitReference].[IsUnitMove], 0) = 1
+                    AND ISNULL([tblUnitReference].[IsTerminated], 0) = 0
+                    AND ISNULL([tblUnitReference].[IsUnitMoveOut], 0) = 0
                     AND CONVERT(VARCHAR(10), GETDATE(), 103) = CONVERT(VARCHAR(10), [tblMonthLedger].[LedgMonth], 103)
                 )
                 OR
                     (
                         ISNULL([tblMonthLedger].[IsHold], 0) = 0
-                        AND ISNULL([tblMonthLedger].[IsPaid], 0) = 0
+                        AND ISNULL([tblMonthLedger].[IsPaid], 0) = 1
+                        AND ISNULL([tblUnitReference].[IsSignedContract], 0) = 1
+                        AND ISNULL([tblUnitReference].[IsUnitMove], 0) = 1
+                        AND ISNULL([tblUnitReference].[IsTerminated], 0) = 0
+                        AND ISNULL([tblUnitReference].[IsUnitMoveOut], 0) = 0
                         AND CONVERT(VARCHAR(10), GETDATE(), 103) = CONVERT(VARCHAR(10), [tblMonthLedger].[LedgMonth], 103)
                     )
     --UNION
